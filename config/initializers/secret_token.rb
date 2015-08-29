26b7bf9259be8d492354d9f68ec7fa3b479ce286
Mariko <maripiyok@gmail.com>
2015-08-30 08:44:06 +0900

    Add rails new files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CopyQiita::Application.config.secret_key_base = '114624eafff18ad89a75fd662c873f4d1432b0b0153fb841137d7e10e7f655aa538ec96769ade3d97800debd50d971c008f8b280806286c46bd1c8e12c808088'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CopyQiita::Application.config.session_store :cookie_store, key: '_copy-qiita_session'
