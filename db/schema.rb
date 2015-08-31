cdcbbbd82951cdd1c8a046c1e9d53810f756f6dd
Mariko <maripiyok@gmail.com>
2015-08-31 22:36:14 +0900

    Add devise omniauthable config setting. DB migration for User model  by adding omniauth related columns.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150831130043) do
+ActiveRecord::Schema.define(version: 20150831132700) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -26,9 +26,15 @@ ActiveRecord::Schema.define(version: 20150831130043) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider",               default: "", null: false
+    t.string   "uid",                    default: "", null: false
+    t.string   "nickname"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["provider"], name: "index_users_on_provider"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["uid"], name: "index_users_on_uid"
 
 end
