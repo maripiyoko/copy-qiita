e8a3e44d537aea1a020738d4ede441b088417422
Mariko <maripiyok@gmail.com>
2015-08-31 23:06:07 +0900

    Add omniauth login controller for github. Add routes for adding controller.
    Add omniauth model method to User model.
    
    Add login link to root page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 CopyQiita::Application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   root 'welcome#index'
   get 'home', to: 'home#show'
 end
