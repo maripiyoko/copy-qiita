eaef6657e7e362983c2b3375be49b7c735857f07
Mariko <maripiyok@gmail.com>
2015-09-01 21:52:02 +0900

    Create Item model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150831132700) do
+ActiveRecord::Schema.define(version: 20150901114424) do
+
+  create_table "items", force: true do |t|
+    t.string   "title"
+    t.text     "contents"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
