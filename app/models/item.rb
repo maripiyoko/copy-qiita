7c7bb14565f642106db7efbe1f70cd2877e928bd
Mariko <maripiyok@gmail.com>
2015-09-02 22:21:33 +0900

    Add Stock model.
    user can have many stocks.
    item can have many stocks.
    user and items can connect each other by stock.
    stock should have unique index for user_id and item_id.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ActiveRecord::Base
   belongs_to :user
+  has_many :stocks
+  has_many :stocking_users, through: :stocks, source: :user
 
   validate :title, presence: true
   validate :contents, presence: true
---
+++ db/migrate/20150902122035_create_stocks.rb
@@ -0,0 +1,11 @@
+class CreateStocks < ActiveRecord::Migration
+  def change
+    create_table :stocks do |t|
+      t.references :item, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
+      t.timestamps
+    end
+
+    add_index :stocks, [:user_id, :item_id], unique: true
+  end
+end
